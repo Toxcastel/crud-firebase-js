fd43fc0194af86d72138f40b743ca405523ac952
albertorapu@gmail.com <albertorapu@gmail.com>
2022-11-14 16:43:16 -0300

    Basic config

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This is just an introductory project to quickly learn the basics of Firebase.
 The tutorial I am using is from this link: https://www.youtube.com/watch?v=ey4k6mW9ds4
+
+It only runs with live server
+
+1. Basic config
+
+- By creating a basic HTML form, at the end of the body tag we'll add type="module" so we can import the functions of firebase.
+- index.js file will have the basic config from the firebase documentation
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBoaJ1jESh-QM0itWUJoNCBbnZzvkDyyB0",
+    authDomain: "crud-firebase-js-e813f.firebaseapp.com",
+    projectId: "crud-firebase-js-e813f",
+    storageBucket: "crud-firebase-js-e813f.appspot.com",
+    messagingSenderId: "655588852855",
+    appId: "1:655588852855:web:71cb56be5aab58fc1d7272",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
